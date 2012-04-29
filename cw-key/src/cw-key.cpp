8b5d9dea8e6fb6036825a867da709f97008f4955
povloid <t34box@gmail.com>
2012-04-29 17:40:54 +0600

    73

--- cw-key/src/cw-key.cpp
+++ cw-key/src/cw-key.cpp
@@ -13,6 +13,8 @@
 #include <pthread.h>
 #include <alsa/asoundlib.h>
 #include <math.h>
+#include <queue>
+
 using namespace std;
 
 /**
@@ -54,6 +56,7 @@ const int ditDashLong = 1;
 const int charLong = 4;
 const int wordLong = 7;
 
+const int A16 = 1;
 const float AMPLITUDE = 10000;
 
 char* device = (char*) "default"; /* playback device */
@@ -61,16 +64,23 @@ snd_output_t *output = NULL;
 snd_pcm_t *handle;
 snd_pcm_sframes_t frames;
 
+struct InputChar {
+public:
+	int skanCode;
+};
+
 class MorseThread: public Thread {
 private:
 
-	map<int, char*> m;
-	unsigned short bufferDit[16 * 1024 * ditLong]; /* dit data */
-	unsigned short bufferDash[16 * 1024 * dashLong]; /* dash data */
+	queue<InputChar> qinput;
 
-	unsigned short bufferDitDashPause[16 * 1024 * ditDashLong];
-	unsigned short bufferCharPause[16 * 1024 * charLong];
-	unsigned short bufferWordPause[16 * 1024 * wordLong];
+	map<int, char*> m;
+	unsigned short bufferDit[A16 * 1024 * ditLong]; /* dit data */
+	unsigned short bufferDash[A16 * 1024 * dashLong]; /* dash data */
+
+	unsigned short bufferDitDashPause[A16 * 1024 * ditDashLong];
+	unsigned short bufferCharPause[A16 * 1024 * charLong];
+	unsigned short bufferWordPause[A16 * 1024 * wordLong];
 
 	void setAll0(unsigned short b[]) {
 		for (unsigned int i = 0; i < sizeof(b) / 2; i++) {
@@ -95,31 +105,61 @@ public:
 		m[9] = (char*) "---..";
 		m[10] = (char*) "----.";
 		m[11] = (char*) "-----";
+
+		// Test
+
+		for (int i = 0; i < 10; i++) {
+			InputChar ic;
+			ic.skanCode = 8;
+			add(ic);
+
+			ic.skanCode = 4;
+			add(ic);
+		}
+	}
+
+	void add(InputChar ic) {
+		qinput.push(ic);
 	}
 
 	void run() {
 
 		int err;
 
-
 		//for (int i = 0; i < 20; i++, sleep(1))
 		//	std::cout << "a  " << std::endl;
 
-		float amplitude = 0;
-		int p = 0;
-		int s = sizeof(bufferDit) / 2;
+		// Заготовки
+		{
+			float amplitude = 0;
+			int p = 0;
+			int s = sizeof(bufferDit) / 2;
+			for (int i = 0; i < s; i++) {
+				if (amplitude < AMPLITUDE && i < (s - p)) {
+					amplitude += 100;
+					p = i;
+				} else if (i > (s - p - 20) && amplitude > 0) {
+					amplitude -= 100;
+				}
 
-		for (int i = 0; i < s; i++) {
-			if (amplitude < AMPLITUDE && i < (s - p)) {
-				amplitude += 100;
-				p = i;
-			} else if (i > (s - p - 20) && amplitude > 0) {
-				amplitude -= 100;
+				bufferDit[i] = sin(((float) i) / 12) * amplitude;
 			}
-
-			bufferDit[i] = sin(((float) i) / 12) * amplitude;
 		}
+		{
+			float amplitude = 0;
+			int p = 0;
+			int s = sizeof(bufferDash) / 2;
+			for (int i = 0; i < s; i++) {
+				if (amplitude < AMPLITUDE && i < (s - p)) {
+					amplitude += 100;
+					p = i;
+				} else if (i > (s - p - 20) && amplitude > 0) {
+					amplitude -= 100;
+				}
 
+				bufferDash[i] = sin(((float) i) / 12) * amplitude;
+			}
+		}
 
 		if ((err = snd_pcm_open(&handle, device, SND_PCM_STREAM_PLAYBACK, 0))
 				< 0) {
@@ -133,41 +173,43 @@ public:
 			exit(EXIT_FAILURE);
 		}
 
-		for (int i = 0; i < 5; i++) {
+		for (;;) {
+			while (!qinput.empty()) {
 
-			frames = snd_pcm_writei(handle, bufferDit, sizeof(bufferDit) / 2);
+				int sk = qinput.front().skanCode;
+				char* simbol = m[sk];
 
-			if (frames < 0)
-				frames = snd_pcm_recover(handle, frames, 0);
-			if (frames < 0) {
-				printf("snd_pcm_writei failed: %s\n", snd_strerror(err));
-				break;
+				while ((*simbol) != 0) {
+					//cout<<*simbol<<'|';
+					char s = *simbol;
+
+					if (s == '.') {
+						frames = snd_pcm_writei(handle, bufferDit,
+								sizeof(bufferDit) / 2);
+					} else if (s == '-') {
+						frames = snd_pcm_writei(handle, bufferDash,
+								sizeof(bufferDash) / 2);
+					}
+
+					frames = snd_pcm_writei(handle, bufferDitDashPause,
+							sizeof(bufferDitDashPause) / 2);
+					simbol++;
+				}
+				//cout<<endl;
+				frames = snd_pcm_writei(handle, bufferCharPause,
+						sizeof(bufferCharPause) / 2);
+				qinput.pop();
 			}
 
-			if (frames > 0 && frames < (long) sizeof(bufferDit) / 2)
-				printf("Short write (expected %li, wrote %li)\n",
-						(long) sizeof(bufferDit) / 2, frames);
-
-			// ---------------------------------------------------------
-
-			frames = snd_pcm_writei(handle, bufferDitDashPause, sizeof(bufferDitDashPause) / 2);
-
-			if (frames < 0)
-				frames = snd_pcm_recover(handle, frames, 0);
-			if (frames < 0) {
-				printf("snd_pcm_writei failed: %s\n", snd_strerror(err));
-				break;
-			}
-
-			if (frames > 0 && frames < (long) sizeof(bufferDitDashPause) / 2)
-				printf("Short write (expected %li, wrote %li)\n",
-						(long) sizeof(bufferDitDashPause) / 2, frames);
+			frames = snd_pcm_writei(handle, bufferWordPause,
+					sizeof(bufferWordPause) / 2);
 
 		}
 
 		snd_pcm_close(handle);
 
 	}
+
 };
 
 /**
